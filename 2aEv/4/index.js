c636731d76e67a9c25348f1e442383b8f52f8611
IrisCampusFP <iris.perezaparicio24@campusfp.es>
2025-12-05 11:06:56 +0100

    index2 listado de productos2

---
+++ 2aEv/4/index.js
@@ -0,0 +1,110 @@
+// let section2 = document.getElementById("section2");
+
+// let titulo = "mi titulo 2"
+
+// let texto = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi saepe, unde nesciunt expedita qui sit dolorem nam inventore repellat maxime atque quaerat nihil, molestiae autem ratione odio eligendi laboriosam corporis?";
+
+// let img = "img/main_image_star-forming_region_carina_nircam_final-5mb.webp";
+
+// let articulo = document.createElement("article");
+
+// let section3 = document.getElementById("section3");
+// let contenedor = document.getElementById("contenedor");
+
+
+// articulo.innerHTML = `
+// <div class="titulo">
+//     <h1>  ${titulo}  </h1>
+// </div>
+
+// <div class="texto">
+//     ${texto}
+// </div>
+
+// <div class="imagen">
+//     <img src="${img}">
+// </div>
+// `;
+
+// articulo.className = "articulo2";
+// section2.appendChild(articulo);
+
+
+//consumir el json
+
+// fetch("productos.json")
+//     .then(response => response.json())
+//     .then(data => {
+
+//         data.productos.forEach(p => {
+
+
+        
+
+// contenedor.innerHTML += `
+//             <div class="card">
+//                 <div class="titulo">
+//                     <h2>${p.nombre}</h2>
+//                 </div>
+//                 <div class="imagen">
+//                     <img class="img" src="${p.imagen}">
+//                 </div>
+//                 <div class="precio">
+//                     <p>Precio: ${p.precio} €</p>
+//                 </div>
+
+//                 <div class="botones"> 
+//                     <button>Comprar</button>
+//                  </div>
+//             </div>
+//         `;
+//             section3.appendChild(contenedor)
+
+//         });//fin del for
+
+
+//     })//fin del then
+//     .catch(e => console.log(e))
+
+
+    /* INDEX 2 */
+    let section1 = document.getElementById("section1")
+
+    let contenedor2 = document.getElementById("contenedor2")
+
+    
+    fetch("productos2.json")
+        .then(respuesta => respuesta.json())
+        .then(data => {
+        data.productos.forEach(p => {
+           
+        
+            contenedor2.innerHTML+=`
+            <div class="card">
+
+                <div class="titulo">
+                    <h3>${p.nombre}</h3>
+                </div>
+
+                <div class="imagen">
+                    <img src="${p.imagen}">
+                </div>
+
+                <div class="precio">
+                    <p>${p.precio} €</p>
+                </div>
+
+                <div class="botones">
+                    <button class="boton" id="${p.id}">COMPRAR</button>
+                </div>
+
+            </div>
+            `;
+
+            section1.appendChild(contenedor2)
+        })
+        
+
+
+    })//fin del then
+    .catch(e => console.log(e))
